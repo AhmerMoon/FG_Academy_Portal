ac6a3cd7fd8108f3d3a7a43124de3752efe3fce1
Ahmer Moon <ahmermoonmajid@gmail.com>
2026-09-20 14:43:28 +0500

    main

---
+++ FGEI_Backend/Send_Attendance_Whatsapp.bat
@@ -0,0 +1,8 @@
+@echo off
+echo 🚀 Starting FGEI Attendance Automation Engine...
+:: Neeche apne Python folder ka exact path likhna jahan main.py pari hai
+cd /d "D:\Path\To\Your\Python\Automation_Folder"
+python main.py
+echo.
+echo ✅ WhatsApp Reports Process Completed.
+pause
